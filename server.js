c2e1798324175ce910e787091d07f1af7be4a827
KVCobbs <kenyacobbs9001@gmail.com>
2020-08-05 19:20:22 -0400

    push

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ db.on("disconnected", () => console.log("mongo disconnected"));
 // CORS SECURITY CONFIGURATIONS
 
 // CREATE A WHITELIST OF WHICH WEBSITES CAN MAKE API CALLS TO YOUR SERVER
-const whitelist = ["http://localhost:3000/", "http://example2.com"];
+const whitelist = ["http://localhost:3000/", "https://insultslibrary.netlify.app/", "https://insultslibrary.herokuapp.com/"];
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
